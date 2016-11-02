8ebe6b76db5e363c5feaaac379b084bbbc3bc19f
Jesse Mortenson <jessemortenson@gmail.com>
2016-11-02 00:29:54 -0500

    Revising to streamline per conversation
    Dropped convfentional-changelog change types, removed unnecessary dependencies, made line lengths more restrictive

--- engine.js
+++ engine.js
@@ -1,24 +1,11 @@
 "format cjs";
 
 var wrap = require('word-wrap');
-var map = require('lodash.map');
-var longest = require('longest');
-var rightPad = require('right-pad');
 
 // This can be any kind of SystemJS compatible module.
 // We use Commonjs here, but ES6 or AMD would do just
 // fine.
 module.exports = function (options) {
-
-  var types = options.types;
-
-  var length = longest(Object.keys(types)).length + 1;
-  var choices = map(types, function (type, key) {
-    return {
-      name: rightPad(key + ':', length) + ' ' + type.description,
-      value: key
-    };
-  });
 
   return {
     // When a user runs `git cz`, prompter will
@@ -33,7 +20,9 @@ module.exports = function (options) {
     // By default, we'll de-indent your commit
     // template and will keep empty lines.
     prompter: function(cz, commit) {
-      console.log('\nLine 1 will be cropped at 100 characters. All other lines will be wrapped after 100 characters.\n');
+      var allowedShort = 50;
+      var allowedLong = 72;
+      console.log('\nLine 1 will be cropped at ' + allowedShort + ' characters. All other lines will be wrapped after ' + allowedLong + ' characters.\n');
 
       // Let's ask some questions of the user
       // so that we can populate our commit
@@ -44,22 +33,30 @@ module.exports = function (options) {
       // collection library if you prefer.
       cz.prompt([
         {
-          type: 'list',
-          name: 'type',
-          message: 'Select the type of change that you\'re committing:',
-          choices: choices
-        }, {
           type: 'input',
           name: 'task',
-          message: 'Enter Teamwork task url, or url in some other task/ticketing system:\n'
+          message: 'Enter Teamwork task url, or url in some other task/ticketing system:\n',
+          validate: function(input) {
+            if (input.length == 0) {
+              return 'A task URL is required.'
+            }
+            return true;
+          }
         }, {
           type: 'input',
           name: 'group',
-          message: 'Add any grouping term or tag you want to apply to this commit:\n'
+          message: '(optional) Add any grouping term or tag you want to apply to this commit:\n'
         }, {
           type: 'input',
           name: 'subject',
-          message: 'Write a short description of the change (shorter than this prompt!):\n'
+          message: 'Write a short description of the change (shorter than this prompt!):\n',
+          validate: function (input) {
+            allowedLength = allowedShort - 11;
+            if (input.length > allowedLength) {
+              return 'Please try again. Your message must be no longer than this: ' + input.substring(0, allowedLength);
+            }
+            return true;
+          }
         }, {
           type: 'input',
           name: 'body',
@@ -67,7 +64,7 @@ module.exports = function (options) {
         }
       ]).then(function(answers) {
 
-        var maxLineWidth = 100;
+        var maxLineWidth = 72;
 
         var wrapOptions = {
           trim: true,
@@ -78,16 +75,27 @@ module.exports = function (options) {
         var group = answers.group.trim();
         group = group ? '::' + answers.group.trim() + ':: ' : '';
 
-        var task = answers.task.trim();
-        task = task ? '\n\nTask: ' + answers.task.trim() : '';
+        // Task URL and generate task code#
+        var taskResponse = answers.task.trim();
+        var taskUrl = taskResponse ? '\n\nTask: ' + taskResponse : '';
+
+        // Derive taskCode from Task URL if we match a known URL signature
+        var taskCode = '';
+        var teamworkSignature = 'projects.origineight.net/tasks/';
+        if (taskResponse && taskResponse.indexOf(teamworkSignature) != -1) {
+          // Format TeamWork task code
+          taskCode = taskResponse.substring(taskResponse.indexOf(teamworkSignature) + teamworkSignature.length + 1);
+          taskCode = (taskCode.indexOf('?') != -1) ? taskCode.substring(0, taskCode.indexOf('?')) : taskCode;
+          taskCode = 'TW #' + taskCode + ' ';
+        }
 
         // Hard limit this line
-        var head = (group + answers.subject.trim()).slice(0, maxLineWidth);
+        var head = (taskCode + group + answers.subject.trim()).slice(0, maxLineWidth);
 
         // Wrap these lines at 100 characters
         var body = wrap(answers.body, wrapOptions);
 
-        commit(head + '\n\n' + body + task + "\tType: " + answers.type);
+        commit(head + '\n\n\n\n' + body + taskUrl);
       });
     }
   };
--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 "format cjs";
 
 var engine = require('./engine');
-var conventionalCommitTypes = require('conventional-commit-types');
 
-module.exports = engine({
-  types: conventionalCommitTypes.types
-});
+module.exports = engine({});
